1b97c0d57cacf1cd5a40328a89d1ccb03682d658
Wennys Camilo <wennys.camilo@mindsdigital.net>
2023-07-25 15:03:24 -0300

    fix: antispoofing mapper

--- lib/src/core/external/mappers/antispoofing_mapper.dart
+++ lib/src/core/external/mappers/antispoofing_mapper.dart
@@ -1,7 +1,7 @@
 import '../../domain/entities/biometrics_reponse/antispoofing.dart';
 
 class AntiSpoofingMapper {
-  AntiSpoofing fromObject(Map<String, dynamic> map) {
+  static AntiSpoofing fromObject(Map<String, dynamic> map) {
     return AntiSpoofing(
       result: map['result'],
       status: map['status'],
--- lib/src/core/external/mappers/details_mapper.dart
+++ lib/src/core/external/mappers/details_mapper.dart
@@ -1,3 +1,5 @@
+import 'package:minds_digital/src/core/external/mappers/antispoofing_mapper.dart';
+
 import '../../domain/entities/biometrics_reponse/details.dart';
 import 'flag_mapper.dart';
 import 'voice_match_mapper.dart';
@@ -9,7 +11,8 @@ class DetailsMapper {
       voiceMatch: map['voice_match'] != null
           ? VoiceMatchMapper.toObject(map['voice_match'] as Map<String, dynamic>)
           : null,
-      antiSpoofing: map['antispoofing'],
+      antiSpoofing:
+          map['antispoofing'] != null ? AntiSpoofingMapper.fromObject(map['antispoofing']) : null,
     );
   }
 }
